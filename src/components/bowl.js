6f228f1fdda24aa91ee6c93cf5d3b75343f18822
Po <8546374-po-man@users.noreply.gitlab.com>
2026-05-05 18:35:39 +0800

    feat(ui): let serve meal button shine if full

--- src/components/bowl.js
+++ src/components/bowl.js
@@ -10,9 +10,12 @@ export function updateBowl(container, gameState, onServeMeal) {
     const bowlHTML = gameState.bowl.map(ing => `<span class="ingredient">${ing.emoji}</span>`).join('');
     const hasIngredients = gameState.bowl.length > 0;
     const isFull = gameState.isBowlFull();
+
+    const buttonClasses = [isFull ? 'shine' : '', !hasIngredients ? 'disabled' : ''].filter(Boolean).join(' ');
+
     container.innerHTML = `
         <div class="ingredients">${bowlHTML}</div>
-        <button id="serve-btn" ${hasIngredients ? '' : 'disabled class="disabled"'}>${isFull ? 'Serve Meal' : 'Serve Meal'}</button>
+        <button id="serve-btn" class="${buttonClasses}" ${hasIngredients ? '' : 'disabled'}>Serve Meal</button>
     `;
     const serveBtn = container.querySelector('#serve-btn');
     if (serveBtn && hasIngredients) {
